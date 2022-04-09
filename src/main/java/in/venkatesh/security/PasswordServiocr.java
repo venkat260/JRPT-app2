fdb78b953f5344f6ad4dd8ad516637e7c966faa9
venkat260 <chigiri.vennkatesh@gmail.com>
2022-04-09 08:59:12 +0530

    first commit

---
+++ src/main/java/in/venkatesh/security/PasswordServiocr.java
@@ -0,0 +1,41 @@
+package in.venkatesh.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordServiocr {
+	
+	
+
+	public static String encode(String txt) {
+
+		Encoder encoder = Base64.getEncoder();
+
+		String encodetxt = encoder.encodeToString(txt.getBytes());// converting the txt into bytes
+
+		return encodetxt;
+
+	}
+
+	public static String decode(String encodetxt) {
+		Decoder decoder = Base64.getDecoder();
+
+		byte[] decode = decoder.decode(encodetxt);// we need to convert the obtained byte code array to string
+
+		return new String(decode);
+	}
+
+	public static void main(String[] args) {
+		
+		String encode = PasswordServiocr.encode("venkatesh");
+		
+		System.out.println(encode);
+		
+		String decode = PasswordServiocr.decode(encode);
+		
+		System.out.println(decode);
+		
+
+	}
+}
